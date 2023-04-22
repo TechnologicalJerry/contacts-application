6ab8a9f895ab37049eddc2b818a921c88abf12c8
technologicaljerry <cserajatverma@gmail.com>
2023-04-22 22:48:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const dotenv = require("dotenv").config()
 
 const app = express();
 
-const port = 5000;
+const port = process.env.SERVER_PORT || 9000;
 
 app.listen(port, () => {
-    console.log(`Node server runnibg on port: ${port}`);
+    console.log(`Node server runnibgs on port: ${port}`);
 })

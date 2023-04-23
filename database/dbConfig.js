c072d24694e750c94edbd6553b1e4a13f0b6dd97
technologicaljerry <cserajatverma@gmail.com>
2023-04-23 23:07:19 +0530

    implement and config database

---
+++ database/dbConfig.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+async function connect() {
+    try {
+        mongoose.connect(process.env.DATABASE_URL);
+        console.log('Mongo DataBase Connected!');
+    } catch (error) {
+        console.log('Database Error', error);
+    }
+}
+
+module.exports = connect();

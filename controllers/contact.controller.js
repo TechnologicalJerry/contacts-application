a52c88e7fe8f8a5f0f0fc3a12278aa4b68fd6027
technologicaljerry <cserajatverma@gmail.com>
2023-04-23 23:23:23 +0530

    Update contact.controller.js

---
+++ controllers/contact.controller.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const expressHandler = require('express-async-handler');
+
+const getAllContactList = expressHandler(async (req, res, next) => {
+    const contacts = 
+})

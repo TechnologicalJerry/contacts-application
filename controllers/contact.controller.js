4d66f53bb6ca44072f72a53bfa63dfb9bfe877af
technologicaljerry <cserajatverma@gmail.com>
2023-04-23 23:30:26 +0530

    Update contact.controller.js

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -2,6 +2,17 @@ const express = require('express');
 
 const expressHandler = require('express-async-handler');
 
+const Contact = require('../database/model');
+
 const getAllContactList = expressHandler(async (req, res, next) => {
-    const contacts = 
+    const contacts = await Contact.find();
+    if (contacts) {
+        console.log('Getting all contacts');
+    } else {
+        console.log('Something wrong!');
+    }
 })
+
+module.exports = {
+    getAllContactList
+}

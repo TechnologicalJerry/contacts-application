fa682f4724fe6186fba6835b667bd6684757dead
Rajat Verma <cserajatverma@gmail.com>
2026-08-28 09:33:57 +0530

    config(vue-client): update nuxt.config.ts with CSS, runtimeConfig, and meta tags

--- vue-client/nuxt.config.ts
+++ vue-client/nuxt.config.ts
@@ -3,14 +3,31 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  css: ['~/assets/css/main.css'],
+
   modules: [
-    '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
     '@nuxt/ui'
-  ]
+  ],
+
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:5050'
+    }
+  },
+
+  app: {
+    head: {
+      title: 'Contacts App - Vue & Nuxt 3 Client',
+      meta: [
+        { name: 'description', content: 'Enterprise Contacts Management Application built with Vue 3, Nuxt 3, and Express backend.' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  }
 })

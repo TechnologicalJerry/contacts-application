e7166d6e497f48a43acfa7c08a632e83545d58e9
Rajat Verma <cserajatverma@gmail.com>
2026-08-28 09:33:54 +0530

    refactor(vue-client): update app.vue to render NuxtPage and loading indicator

--- vue-client/app.vue
+++ vue-client/app.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="min-h-screen flex flex-col bg-[#0b0f19] text-gray-100 font-sans">
+    <NuxtLoadingIndicator color="#6366f1" :height="3" />
+    <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+const { initUserFromToken } = useAuth()
+
+onMounted(() => {
+  initUserFromToken()
+})
+</script>
